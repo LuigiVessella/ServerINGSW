669e32a893b3f214c6ae62bd4d6682863a1e0fb3
Luigi Vessella <vessellaluigi12@gmail.com>
2023-01-29 18:49:49 +0100

    login e registrazione

--- ratatuille/src/main/java/com/example/ingsw2022/ratatuille/Controller/AdminController.java
+++ ratatuille/src/main/java/com/example/ingsw2022/ratatuille/Controller/AdminController.java
@@ -2,7 +2,6 @@ package com.example.ingsw2022.ratatuille.Controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -31,16 +30,25 @@ public class AdminController {
 
      
     @PostMapping("/addNew")
-    public @ResponseBody String addNewAdmin(@RequestParam String partitaIva, @RequestParam String nome, @RequestParam String cognome) {
+    public @ResponseBody String addNewAdmin(@RequestParam String partitaIva, @RequestParam String nome, @RequestParam String cognome, @RequestParam String email) {
         Admin admin = new Admin();
         admin.setPartita_iva(partitaIva);
         admin.setNome(nome);
         admin.setCognome(cognome);
+        admin.setEmail(email);
         adminRepository.save(admin);
-        return "Succefully saved";
-        
+        return "Succefully saved"; 
     }
 
+    @PostMapping("/login") 
+    public @ResponseBody String adminLogin(@RequestParam String email){
+        Admin admin = new Admin();
+        admin = adminRepository.findByEmailAddress(email);
+        if(admin.getNome().isEmpty()) return "Utente non esiste";
+        else return "utente esiste";
+    }
+
+    
     @OnError 
     public @ResponseBody String onError(){
         return "errore";
