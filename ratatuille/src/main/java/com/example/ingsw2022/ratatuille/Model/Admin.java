46ad041665996432d4cf11a6079f7e561c6dde6e
Luigi Vessella <vessellaluigi12@gmail.com>
2023-01-31 19:19:42 +0100

    aaa

--- ratatuille/src/main/java/com/example/ingsw2022/ratatuille/Model/Admin.java
+++ ratatuille/src/main/java/com/example/ingsw2022/ratatuille/Model/Admin.java
@@ -30,6 +30,7 @@ public class Admin {
     }
 
     public Admin() {
+        
     }
 
 
--- ratatuille/src/main/java/com/example/ingsw2022/ratatuille/Model/Ristorante.java
+++ ratatuille/src/main/java/com/example/ingsw2022/ratatuille/Model/Ristorante.java
@@ -97,8 +97,4 @@ public class Ristorante {
     public void setCamerieri(Set<Cameriere> camerieri) {
         this.camerieri = camerieri;
     }
-    
-
-    
-
 }
